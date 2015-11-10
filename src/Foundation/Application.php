d571099fec2d3556e5fe5745f5f689ea46bdec32
chongyi <xpz3847878@163.com>
2015-11-10 22:04:40 +0800

    init

---
+++ src/Foundation/Application.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Application.php
+ *
+ * Created by Chongyi
+ * Date & Time 2015/11/10 22:00
+ */
+
+namespace Ancell\Framework\Foundation;
+
+use Illuminate\Foundation\Application as IlluminationApplication;
+
+class Application extends IlluminationApplication
+{
+    
+}
